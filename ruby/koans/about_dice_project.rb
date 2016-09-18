c4ee3a506aa7abb5a50de297cc5594b86857fbbf
Rodrigo De Frutos <darkrodry@gmail.com>
2016-09-18 15:51:41 +0200

    Create class dice to fix dice tests

--- ruby/koans/about_dice_project.rb
+++ ruby/koans/about_dice_project.rb
@@ -1,10 +1,14 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
-#
-# class DiceSet
-#   code ...
-# end
+
+class DiceSet
+  attr_reader :values
+
+  def roll(times)
+    @values = (1..times).map { Random.rand(6) + 1 }
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
