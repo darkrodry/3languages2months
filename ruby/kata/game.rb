f55734de0a0afe600c7be8b646a2ad8aa09c9361
Rodrigo De Frutos <darkrodry@gmail.com>
2016-09-19 22:03:08 +0200

    Create score method in game

--- ruby/kata/game.rb
+++ ruby/kata/game.rb
@@ -1,7 +1,13 @@
 class Game
+
   def initialize
   end
 
   def roll(pins)
   end
+
+  def score
+    return 0
+  end
+
 end
--- ruby/kata/game_test.rb
+++ ruby/kata/game_test.rb
@@ -7,5 +7,6 @@ class GameTest < Test::Unit::TestCase
     20.times do
       game.roll(0)
     end
+    assert_equal 0, game.score
   end
 end
