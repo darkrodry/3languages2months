b912efbe5f02a0ea26c73cc7ca5f24ed8d5269fe
Rodrigo De Frutos <darkrodry@gmail.com>
2016-09-20 00:05:17 +0200

    Create strike logic

--- ruby/kata/game.rb
+++ ruby/kata/game.rb
@@ -2,7 +2,7 @@ class Game
 
   def initialize
     @current_roll = 0
-    @rolls = []
+    @rolls = Array.new(21) {0}
   end
 
   def roll(pins)
@@ -12,17 +12,39 @@ class Game
 
   def score
     score = 0
-    for frame in (0..19).step(2) do  
-      if is_spare?(frame)
-        score += 10 + @rolls[frame+2]
+    frame = 0
+    10.times do
+      if is_strike?(frame)
+        score += 10 + count_strike_bonus(frame)
+        frame += 1
+      elsif is_spare?(frame)
+        score += 10 + count_spare_bonus(frame)
+        frame += 2
       else
-        score += @rolls[frame] + @rolls[frame+1]
+        score += count_frame_points(frame)
+        frame += 2
       end
     end
     return score
   end
 
+  def is_strike?(frame)
+    return @rolls[frame] == 10
+  end
+
   def is_spare?(frame)
     return @rolls[frame] + @rolls[frame+1] == 10
+  end
+
+  def count_frame_points(frame)
+    return @rolls[frame] + @rolls[frame+1]
+  end
+
+  def count_spare_bonus(frame)
+    return @rolls[frame+2]
+  end
+
+  def count_strike_bonus(frame)
+    return @rolls[frame+1] + @rolls[frame+2]
   end
 end
