7f4b1a2731a48ecdf85c82185f8f9cb6dfa22f6e
Rodrigo De Frutos <darkrodry@gmail.com>
2016-09-19 22:49:54 +0200

    Refactor to add an array to cache the score

--- ruby/kata/game.rb
+++ ruby/kata/game.rb
@@ -1,15 +1,19 @@
 class Game
 
   def initialize
-    @score = 0
+    @current_roll = 0
+    @rolls = []
   end
 
   def roll(pins)
-    @score += pins
+    @rolls[@current_roll] = pins
+    @current_roll += 1
   end
 
   def score
-    return @score
+    s = 0
+    @rolls.each { |pins| s += pins }
+    return s
   end
 
 end
