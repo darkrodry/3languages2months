e2c9481246de60c0fe532823d3ebdf5e4fba255d
Rodrigo De Frutos <darkrodry@gmail.com>
2016-09-19 21:55:47 +0200

    Add roll method to game

--- ruby/kata/game.rb
+++ ruby/kata/game.rb
@@ -1,4 +1,7 @@
 class Game
   def initialize
   end
+
+  def roll(pins)
+  end
 end
--- ruby/kata/game_test.rb
+++ ruby/kata/game_test.rb
@@ -4,5 +4,8 @@ require_relative 'game'
 class GameTest < Test::Unit::TestCase
   def test_gutter_game
     game = Game.new
+    20.times do
+      game.roll(0)
+    end
   end
 end
