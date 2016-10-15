1afc7ed6b48e830637c51bc99c01f819b596e295
Rodrigo De Frutos <darkrodry@gmail.com>
2016-10-15 15:46:32 +0200

    Add spare logic to get-score function

--- clojure/kata/src/kata/game.clj
+++ clojure/kata/src/kata/game.clj
@@ -1,5 +1,17 @@
 (ns kata.game)
 
+(defn spare? [roll]
+	(= 10 (reduce + (take 2 roll)))
+)
+
 (defn get-score [pins]
-  (reduce + pins)
+  (loop [cnt pins acc 0]
+  	(if (> (count cnt) 0)
+	  	(if (spare? cnt)
+	  		(recur (drop 2 cnt) (+ acc (reduce + (take 3 cnt))))
+	  		(recur (drop 2 cnt) (+ acc (reduce + (take 2 cnt))))
+	  	)
+	  	acc
+  	)
+  )
 )
--- clojure/kata/test/kata/game_test.clj
+++ clojure/kata/test/kata/game_test.clj
@@ -9,3 +9,7 @@
 (deftest test-all-ones
     (is (= 20 (get-score (repeat 20 1))))
 )
+
+(deftest test-one-spare
+	(is (= 16 (get-score (concat [5 5 3] (repeat 17 0)))))
+)
