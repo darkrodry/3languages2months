3d3f10647ec56da8bc7986e08235c449ce337e7c
Rodrigo De Frutos <darkrodry@gmail.com>
2016-10-15 17:20:51 +0200

    Fix logic to calculate the score for a 10 round game

--- clojure/kata/src/kata/game.clj
+++ clojure/kata/src/kata/game.clj
@@ -9,13 +9,13 @@
 )
 
 (defn get-score [pins]
-  (loop [cnt pins acc 0]
-  	(if (> (count cnt) 0)
+  (loop [cnt pins acc 0 round 10]
+  	(if (> round 0)
   		(if (strike? cnt)
-  			(recur (drop 1 cnt) (+ acc (reduce + (take 3 cnt))))
+  			(recur (drop 1 cnt) (+ acc (reduce + (take 3 cnt))) (- round 1))
 		  	(if (spare? cnt)
-		  		(recur (drop 2 cnt) (+ acc (reduce + (take 3 cnt))))
-		  		(recur (drop 2 cnt) (+ acc (reduce + (take 2 cnt))))
+		  		(recur (drop 2 cnt) (+ acc (reduce + (take 3 cnt))) (- round 1))
+		  		(recur (drop 2 cnt) (+ acc (reduce + (take 2 cnt))) (- round 1))
 		  	)
 	  	)
 	  	acc
